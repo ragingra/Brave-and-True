3bc25f1ffd8ee367bdafa520f7fa7fd8acf08217
Neil Anderson <neil.anderson@puppet.com>
2022-08-08 10:45:40 +0100

    (PE-34264) chapter one
    This commit initializes the clojure app
    that will be used for my clojure learning plan.

---
+++ clojure-learning-plan/test/clojure_learning_plan/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-learning-plan.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-learning-plan.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
